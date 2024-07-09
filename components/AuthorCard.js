3ae5cec886da4ecf998a4d415e5164f140397440
asparadise33 <abby.paradise@gmail.com>
2024-07-09 13:23:21 -0500

    author's fully updated, added to NAVBAR and dynamic keys updated

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -3,29 +3,24 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
-import { deleteSingleAuthor } from '../api/authorData';
-// import { deleteBook } from '../api/bookData';
+import { deleteAuthorBooks } from '../api/mergedData';
 
 function AuthorCard({ authorObj, onUpdate }) {
-  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
-  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
   const deleteThisAuthor = () => {
-    if (window.confirm(`Delete ${authorObj.title}?`)) {
-      deleteSingleAuthor(authorObj.firebaseKey).then(() => onUpdate());
+    if (window.confirm(`Delete ${authorObj.first_name}?`)) {
+      deleteAuthorBooks(authorObj.firebaseKey).then(() => onUpdate());
     }
   };
 
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
-      <Card.Img variant="top" src={authorObj.image} alt={authorObj.title} style={{ height: '400px' }} />
       <Card.Body>
-        <Card.Title>{authorObj.title}</Card.Title>
-        {/* <p className="card-text bold">{authorObj.sale && <span>SALE<br /></span> } ${authorObj.price}</p>
-        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Card.Title>{authorObj.first_name} {authorObj.last_name}</Card.Title>
+        <p>{authorObj.email}</p>
+        <p className="card-text bold">{authorObj.favorite && <span>Favorite<br /></span> } ${authorObj.favorite}</p>
         <Link href={`/author/${authorObj.firebaseKey}`} passHref>
           <Button variant="primary" className="m-2">VIEW</Button>
         </Link>
-        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
         <Link href={`/author/edit/${authorObj.firebaseKey}`} passHref>
           <Button variant="info">EDIT</Button> */
         </Link>
@@ -39,10 +34,10 @@ function AuthorCard({ authorObj, onUpdate }) {
 
 AuthorCard.propTypes = {
   authorObj: PropTypes.shape({
-    image: PropTypes.string,
-    title: PropTypes.string,
-    sale: PropTypes.bool,
-    price: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    favorite: PropTypes.bool,
+    email: PropTypes.string,
     firebaseKey: PropTypes.string,
   }).isRequired,
   onUpdate: PropTypes.func.isRequired,
--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -2,37 +2,32 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
 
 export default function ViewAuthor() {
   const [authorDetails, setAuthorDetails] = useState({});
   const router = useRouter();
-
-  // TODO: grab firebaseKey from url
   const { firebaseKey } = router.query;
 
-  // TODO: make call to API layer to get the data
   useEffect(() => {
     viewAuthorDetails(firebaseKey).then(setAuthorDetails);
   }, [firebaseKey]);
 
   return (
     <div className="mt-5 d-flex flex-wrap">
-      <div className="d-flex flex-column">
-        <img src={authorDetails.image} alt={authorDetails.title} style={{ width: '300px' }} />
-      </div>
       <div className="text-white ms-5 details">
         <h5>
-          {authorDetails.title} by {authorDetails.authorObject?.first_name} {authorDetails.authorObject?.last_name}
-          {authorDetails.authorObject?.favorite ? ' 🤍' : ''}
+          By {authorDetails.first_name} {authorDetails.last_name}
+          {authorDetails.favorite ? ' 🤍' : ''}
         </h5>
-        Author Email: <a href={`mailto:${authorDetails.authorObject?.email}`}>{authorDetails.authorObject?.email}</a>
+        Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
         <p>{authorDetails.description || ''}</p>
         <hr />
-        <p>
-          {authorDetails.sale
-            ? `🏷️ Favorite $${authorDetails.price}`
-            : `$${authorDetails.price}`}
-        </p>
+        <div className="d-flex flex-wrap">
+          {authorDetails.books?.map((book) => (
+            <BookCard key={book.firebaseKey} bookObj={book} onUpdate={viewAuthorDetails} />
+          ))}
+        </div>
       </div>
     </div>
   );
